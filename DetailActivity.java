7cb3e29c4aa1c004438ce99b732b11668fa0b3f4
kazu0821 <49219453+kazu0821@users.noreply.github.com>
2019-12-01 12:43:26 -0800

    Add files via upload

---
+++ DetailActivity.java
@@ -0,0 +1,55 @@
+package com.example.shoppingcartnew;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView txt1 , txt2, txt3, price1, price2, price3, total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent it=getIntent();
+
+        int val[]=it.getIntArrayExtra("values");
+
+        for(int i=0;i<val.length;i++)
+        {
+            Log.w("Activity","Values: "+val[i]);
+        }
+
+        txt1=findViewById(R.id.textView10);
+        txt1.setText(val[0]+"");
+
+        txt2=findViewById(R.id.textView11);
+        txt2.setText(val[1]+"");
+
+        txt3=findViewById(R.id.textView12);
+        txt3.setText(val[2]+"");
+
+
+        price1 = findViewById(R.id.textView13);
+        price1.setText("$"+val[0] *1.29 +"");
+
+        price2 = findViewById(R.id.textView14);
+        price2.setText("$"+val[1] *2.34 +"");
+
+        price3 = findViewById(R.id.textView15);
+        price3.setText("$"+val[2] *1.15 +"");
+
+        total = findViewById(R.id.textView17);
+
+        float tomato = Float.parseFloat(price1.getText().toString().substring(1));
+        float peach =Float.parseFloat(price2.getText().toString().substring(1));
+        float squash = Float.parseFloat(price1.getText().toString().substring(1));
+
+        total.setText("$"+(tomato + peach+ squash)+"");
+
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.shoppingcartnew;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button btn;
+    EditText txt1;
+
+    ListView list;
+
+    String[] maintitle ={
+            "Peach","Tomato",
+            "Squash"
+    };
+
+    String[] subtitle ={
+            "$2.34","$1.29",
+            "$1.15"
+    };
+
+    Integer[] imgid={
+            R.drawable.peach,
+            R.drawable.tomato,
+            R.drawable.squash
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        btn=(Button)findViewById(R.id.button);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
+                if(position == 0) {
+                    //code specific to first list item
+                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it=new Intent(getBaseContext(),DetailActivity.class);
+
+                int val[]=new int[3];
+
+                for(int i=0;i<list.getCount();i++)
+                {
+                    View view=list.getChildAt(i);
+                    txt1=view.findViewById(R.id.quantity);
+                    int a=Integer.parseInt(String.valueOf(txt1.getText()));
+                    val[i]=a;
+                }
+
+                it.putExtra("values",val);
+                startActivity(it);
+
+
+            }
+        });
+
+
+    }
+}
